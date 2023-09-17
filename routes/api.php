c6de36e5c5ff5802c5932291f4edcf8bf2214c5a
prasetyo <tryprasetyoadi2420@gmail.com>
2023-09-17 17:00:36 +0700

    csrfv2

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,6 @@
 <?php
 
 use App\Http\Controllers\API\AuthController;
-use App\Http\Controllers\Auth\AuthenticatedSessionController;
-use App\Http\Middleware\Authenticate;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ClientController;
