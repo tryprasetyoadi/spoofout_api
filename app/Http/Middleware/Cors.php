c30b1c2e0f62b19191a0a6b249f9e85891b8a78f
Fariz Akbar <fariz313.akbar@gmail.com>
2023-09-18 17:55:37 +0700

    Jos

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -1,21 +1,21 @@
 <?php
-
 namespace App\Http\Middleware;
 
 use Closure;
 
-class Cors 
+class Cors
 {
     /**
-     * Get the path the user should be redirected to when they are not authenticated.
+     * @param $request
+     * @param Closure $next
+     * @return mixed
      */
     public function handle($request, Closure $next)
     {
-        return $next($request)
-            ->header('Access-Control-Allow-Origin', '*')
-            ->header('Access-Control-Allow-Methods', '*')
-            ->header('Access-Control-Allow-Credentials', true)
-            ->header('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type,X-Token-Auth,Authorization')
-            ->header('Accept', 'application/json');
+        $response = $next($request);
+        $response->headers->set('Access-Control-Allow-Origin', '*');
+        $response->headers->set('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT, DELETE');
+        $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Accept, Authorization, X-Requested-With, Application', 'ip');
+        return $response;
     }
 }
